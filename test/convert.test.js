6d1cad1c5a7abb428ed588d3aea3028b91ca8ef4
Greg Allen <me@jga.me>
2012-07-26 10:50:56 -0700

    tests for jade as input file

--- test/convert.test.js
+++ test/convert.test.js
@@ -55,4 +55,32 @@ suite('convert', function() {
     })
     test('convert with html template')
   });
+
+  suite('jade input file', function() {
+
+    test('jade input file with no template', function(done) {
+      convert({
+        input: fixturePath + 'basic.jade',
+        data: {
+          pageTitle: 'Page Title'
+        }
+      }, function(err, results) {
+        assert.equal(results, readFixture('jade4.html'));
+        done();
+      });
+    });
+
+    test('jade input file with a template', function(done) {
+      convert({
+        input: fixturePath + 'basic.jade',
+        template: fixturePath + 'layout2.jade',
+        data: {
+          pageTitle: 'Page Title'
+        }
+      }, function(err, results) {
+        assert.equal(results, readFixture('jade5.html'));
+        done();
+      });
+    });
+  });
 });
