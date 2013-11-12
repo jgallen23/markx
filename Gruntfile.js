b6fbe1cf1bfa52cbfb80a7319c32131053a45788
Greg Allen <me@jga.me>
2013-11-12 11:32:41 -0800

    updated highlight.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,5 @@ module.exports = function(grunt) {
   require('load-grunt-config')(grunt);
 
   grunt.registerTask('default', ['jshint', 'simplemocha']);
-  grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('dev', ['default', 'watch']);
 };
